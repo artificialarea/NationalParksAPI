6312d597078082ddfc427b57b34ac216d4f3fe9e
Sacha Sedriks <sachased@gmail.com>
2020-04-29 22:28:49 -0700

    added addresses to render.

--- script.js
+++ script.js
@@ -2,19 +2,26 @@
 
 //////////////////////////////////////////////////////////////
 // SEPARATION OF CONCERNS: TYPES OF FUNCTIONS
-// Miscellaneous (incl Fetch Requests)
+// Miscellaneous 
+// Fetch Requests
 // Template Generators
 // Rendering Functions
 // Event Handlers
+// For State Glossary
 //////////////////////////////////////////////////////////////
 
-// INIT
+
+
+// MISCELLANEOUS /////////////////////////////////////////////
+
 function init() {
   handleSubmission();
   renderDropdownMenu();
 }
 
-// MISCELLANEOUS /////////////////////////////////////////////
+
+
+// FETCH REQUESTS ////////////////////////////////////////////
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params).map( key => {
@@ -24,7 +31,6 @@ function formatQueryParams(params) {
 }
 
 function fetchStateParkInfo(states, maxResults) {
-  // console.log(states, maxResults);
 
   const apiKey = '5sSsm7fFCYCquxRBY5P0IVUu9Y1OX70vBJb4algf';
   const baseURL = 'https://developer.nps.gov/api/v1/parks';
@@ -32,15 +38,13 @@ function fetchStateParkInfo(states, maxResults) {
   const params = {
     stateCode: states,
     limit: maxResults,
-    // extra param not in requirements
-    // per: https://www.nps.gov/subjects/developer/faqs.htm#CP_JUMP_5619871
     fields: 'images',   
     api_key: apiKey,
   };
 
   const queryString = formatQueryParams(params);
   const url = baseURL + '?' + queryString;
-  // console.log(url);
+
   
   // BECAUSE OFTEN IT TAKES SO LONG FOR THE PROMISE TO BE FULFILLED
   // INFORMING THE USER TO BE PATIENT ;P
@@ -48,14 +52,12 @@ function fetchStateParkInfo(states, maxResults) {
 
   fetch(url)
   .then(response => {
-    // console.log('fetch promise fulfilled');
     if(!response.ok) {
       throw new Error(response.statusText);
     }
     return response.json();
   })
   .then(data => {
-    // console.log(data);
     renderStateParkInfo(data);
     $('.js-please-wait').addClass('hidden').html('');
   })
@@ -71,13 +73,29 @@ function fetchStateParkInfo(states, maxResults) {
 
 function generateStateParkInfo(dataInfo) {
   const arr = [];
+
   for (let i = 0; i < dataInfo.data.length; i++) {
+    
+    const physicalAddress = [];
+    for (let j = 0; j < dataInfo.data[i].addresses.length; j++) {
+      if (dataInfo.data[i].addresses[j].type === 'Physical') {
+        physicalAddress.push(`
+        ${dataInfo.data[i].addresses[j].line1}, 
+        ${dataInfo.data[i].addresses[j].city},
+        ${dataInfo.data[i].addresses[j].stateCode},
+        ${dataInfo.data[i].addresses[j].postalCode}
+        `);
+      }
+    }
+    const strPhysicalAddress = physicalAddress.join('');
+    strPhysicalAddress.replace(/\, /g, '');
+
     arr.push(`<li> 
         <h3 class="park-name">${dataInfo.data[i].fullName}</h3>
         <img src="${dataInfo.data[i].images[0].url}" alt="${dataInfo.data[i].images[0].caption}">
         <p class="park-description"><b>${dataInfo.data[i].states}</b> ${dataInfo.data[i].description}</p>
-        <p class="park-website"><a href="${dataInfo.data[i].url}" target="_blank">${dataInfo.data[i].url}</a></p>
-        <p class="park-address"></p>
+        <p class="park-website"><b>Website:</b> <a href="${dataInfo.data[i].url}" target="_blank">${dataInfo.data[i].url}</a></p>
+        <p class="park-address"><b>Address:</b> ${strPhysicalAddress}</p>
       </li>
     `);
   }
@@ -97,6 +115,7 @@ function renderStateParkInfo(dataInfo) {
 
 
 // EVENT HANDLERS ////////////////////////////////////////////
+
 function handleSubmission() {
   $('#search-form').on('submit', event => {
     event.preventDefault();
@@ -130,133 +149,6 @@ function renderDropdownMenu() {
 }
 
 
-// INVOKE INIT
+
+// document.ready
 $(init);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// _                  _         _                   
-// | |                | |       | |                  
-// | |_ ___  _ __ ___ | |__  ___| |_ ___  _ __   ___ 
-// | __/ _ \| '_ ` _ \| '_ \/ __| __/ _ \| '_ \ / _ \
-// | || (_) | | | | | | |_) \__ \ || (_) | | | |  __/
-//  \__\___/|_| |_| |_|_.__/|___/\__\___/|_| |_|\___|
-
-// GRAVEYARD /////////////////////////////////////////////
-
-// 1. SELECT STATE VIA DROPDOWN //////////////////////////
-
-/*
-
-function generateDropdownMenu(STATES) {
-  const options = STATES.map(item => {
-    return `<option value=${item.abbreviation}>${item.name}</option>
-    `;
-  });
-  return `<option value="" disabled selected>Select a State</option>
-    ${options}
-  `;
-}
-
-function renderDropdownMenu() {
-  const listOfStates = generateDropdownMenu(STATES);
-  $('#js-select-state').html(listOfStates);
-}
-
-
-
-
-
-
-*/
-
-// 2. GET ADDRESS ////////////////////////////////////////
-
-/*
-
-function renderStateParkAddress(dataInfo) {
-  console.log(JSON.stringify(dataInfo));
-  // $('#js-address-results').append(
-  //   `<li>
-  //       <p>${dataInfo.results[0].formatted_address}</p>
-  //   </li>`
-  // );
-
-  $('#js-address-test').removeClass('hidden');
-
-}
-
-function fetchAddress(latitude, longitude) {
-
-  const apiKey =  'removed so not public';
-  const baseURL = 'https://maps.googleapis.com/maps/api/geocode/json';
-
-  // should I use Headers to pass apiKey instead via fetch(url, options)?
-  const options = {
-    headers: new Headers({
-      'X-Api-Key': apiKey})   
-  };
-
-  const params = {
-    key: apiKey,
-    latlng: latitude + ',' + longitude,
-  };
-  
-  const queryString = formatQueryParams(params);
-  
-  const url = baseURL + '?' + queryString;
-  console.log(url);
-
-  fetch(url)
-  .then(response => response.json)
-  .then(data => {
-    console.log(data);
-    // renderStateParkAddress(data);
-  })
-  .catch(err => console.log(err));
-
-}
-
-
-// <excerpt within renderStateParkInfo(dataInfo)>
-
-  for (let i=0; i < dataInfo.data.length; i++) {
-
-    // get latitude and longitude to reverse geocode the address
-    // via Google Maps Geocode API
-    const latitude = dataInfo.data[i].latitude;
-    const longitude = dataInfo.data[i].longitude;
-    // fetchAddress(latitude, longitude);
-    // ^^^^^^^^^^^^^^
-    // Suspending attempt to get address because exeeded timebox
-    // ** REFACTOR ** later
-  
-// </excerpt>
-
-function formatQueryParams(params) {
-  const queryItems = Object.keys(params).map( key => {
-    return `${key}=${params[key]}`;
-
-    // had to remove encodeURIComponent 
-    // because for google maps URL it converts the comma (,)
-    // in latlng to %2C 
-    // e.g. ...latlng=37.81005871%2C-122.4244415
-    // return `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`;
-  });
-  return queryItems.join('&');  
-}
-
-*/
-
-
-
